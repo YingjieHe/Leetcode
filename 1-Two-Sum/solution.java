00cbf3c358fe3fba426659005ede38655de594fe
Jay He <heyingjie1993@outlook.com>
2016-03-29 23:13:15 -0700

    Leetcode

---
+++ 1-Two-Sum/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return null;
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                res[0] = map.get(nums[i]);
+                res[1] = i;
+            }
+            else{
+                map.put(target - nums[i], i);
+            }
+        }
+        return res;
+    }
+}
