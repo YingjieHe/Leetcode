a5819ff33fc1c81100ea7003957c64c155471e72
Jay He <heyingjie1993@outlook.com>
2016-04-01 23:04:34 -0700

    Leetcode

--- 248-Strobogrammatic-Number-III/solution.java
+++ 248-Strobogrammatic-Number-III/solution.java
@@ -1,43 +1,28 @@
 public class Solution {
+    char[][] pairs = {{'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
     int count = 0;
-    String[][] pairs = {{"0", "0"}, {"1", "1"}, {"6", "9"}, {"8", "8"}, {"9", "6"}};
-
-
-  public int strobogrammaticInRange(String low, String high) {
-          Map<Integer, List<String>> map = new HashMap<Integer, List<String>> ();
-          map.put(0, new ArrayList<String>(Arrays.asList("")));
-          map.put(1, new ArrayList<String>(Arrays.asList("0", "1", "8")));
-          for (int len = low.length(); len <= high.length(); len++){
-              helper(len, map, low, high);
-          }
-          return count;
-  }
-         
-              List<String> helper(int n, Map<Integer, List<String>> map, String low, String high){
-              List<String> res = new ArrayList<String>();
-              if (map.containsKey(n)) res = map.get(n);
-              else {
-                  List<String> list = map.containsKey(n - 2) ?  map.get(n - 2) : helper(n - 2, map, low, high);
-              for (int i = 0; i < list.size(); i++){
-                  String s = list.get(i);
-              
-              for (int j = 0; j < pairs.length; j++){
-                  String v = pairs[j][0] + s + pairs[j][1];
-              if (v.length() == high.length() && v.compareTo(high) > 0) break;
-              res.add(v);
-              
-              }
-              }
-              map.put(n, res);
-              }
-              if (n >= low.length()) {
-                  count += res.size();
-              for (String s: res){
-                  if ((s.length() > 1 && s.charAt(0) == '0') ||
-                  (s.length() == high.length() && s.compareTo(high) > 0) ||
-                  (s.length() == low.length() && s.compareTo(low) < 0)) count--;
-              }
-              }
-              return res;
-}
+    
+    public int strobogrammaticInRange(String low, String high) {
+        for(int len = low.length(); len <= high.length(); len++) {
+            dfs(low, high, new char[len], 0, len - 1);
+        }
+        return count;
+    }
+    
+    public void dfs(String low, String high, char[] c, int left, int right) {
+        if(left > right) {
+            String s = new String(c);
+            if((s.length() == low.length() && s.compareTo(low) < 0) || 
+               (s.length() == high.length() && s.compareTo(high) > 0)) return;
+            count++; 
+            return;
+        }
+    
+        for(char[] p : pairs) {
+            c[left] = p[0]; 
+            c[right] = p[1];
+            if(c.length != 1 && c[0] == '0') continue;
+            if(left < right || left == right && p[0] == p[1]) dfs(low, high, c, left + 1, right - 1);
+        }
+    }
 }
