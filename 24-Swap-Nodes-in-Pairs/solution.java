41394246cac9d561c7aa6f43fdf2ec799b4233fa
Jay He <heyingjie1993@outlook.com>
2016-04-03 00:41:59 -0700

    Leetcode

--- 24-Swap-Nodes-in-Pairs/solution.java
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -8,10 +8,18 @@
  */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode n = head.next;
-        head.next = swapPairs(head.next.next);
-        n.next = head;
-        return n;
+         if(head == null || head.next == null) return head;
+         ListNode dummy = new ListNode(0);
+         dummy.next = head;
+         ListNode h = dummy;
+         while(h.next != null && h.next.next != null){
+             ListNode temp = h;
+             h = h.next;
+             temp.next = h.next;
+             ListNode t = h.next.next;
+             h.next.next = h;
+             h.next = t;
+         }
+         return dummy.next;
     }
 }
