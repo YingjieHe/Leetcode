1a2c20a3eb4708d299f34dde543f550a35ca41f6
Jay He <heyingjie1993@outlook.com>
2016-04-02 22:18:16 -0700

    Leetcode

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,13 +1,19 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        for(int i = 0; i < p.length(); s = s.substring(1)){
-            char c = p.charAt(i);
-            if(i + 1 < p.length() && p.charAt(i + 1) == '*'){
-                if(isMatch(s, p.substring(i + 2))) return true;
+        int sl = s.length(), pl = p.length();
+        boolean[][] dp = new boolean[sl + 1][pl + 1];
+        dp[0][0] = true;
+        for(int i = 0; i <= sl; i++){
+            for(int j = 1; j <= pl; j++){
+                char c = p.charAt(j - 1);
+                if(c != '*'){
+                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (c == '.' || c == s.charAt(i - 1));
+                }
+                else{
+                    dp[i][j] = (j > 1 && dp[i][j - 2]) || (i > 0 && dp[i - 1][j] && (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)));
+                }
             }
-            else i++;
-            if(s.length() == 0 || (c != '.' && c != s.charAt(0))) return false;
         }
-        return s.length() == 0;
+        return dp[sl][pl];
     }
 }
