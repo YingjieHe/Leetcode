6f79bdab7a666c57d9b6b21e52b72972780deb7d
Jay He <heyingjie1993@outlook.com>
2016-04-06 20:40:29 -0700

    Leetcode

---
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int longestValidParentheses(String s) {
+        if(s == null || s.length() == 0) return 0;
+        int max = 0;
+        int left = -1;
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') stack.push(i);
+            else{
+                if(stack.isEmpty()) left = i;
+                else{
+                    stack.pop();
+                    if(stack.isEmpty()) max = Math.max(max, i - left);
+                    else max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+}
