a349cb8b3486d8d0a01f0be4d8766ff12029b9e9
Jay He <heyingjie1993@outlook.com>
2016-04-07 09:54:48 -0700

    Leetcode

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public int addDigits(int num) {
-        int res = 0;
-        while(num / 10 > 0){
+        int res = num;
+        while(res / 10 > 0){
+            res = 0;
             while(num > 0){
                 res += num % 10;
                 num /= 10;
             }
             num = res;
-            res = 0;
         }
-        return num;
+        return res;
     }
 }
