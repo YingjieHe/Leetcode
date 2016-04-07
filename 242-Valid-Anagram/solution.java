b584f2fcea501ca9a60cd0433e6bf846ba624d05
Jay He <heyingjie1993@outlook.com>
2016-04-07 10:13:57 -0700

    Leetcode

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        int[] temp = new int[256];
+        int[] count = new int[256];
         for(int i = 0; i < s.length(); i++){
-            temp[s.charAt(i)]++;
-            temp[t.charAt(i)]--;
+            count[s.charAt(i)]++;
+            count[t.charAt(i)]--;
         }
-        for(int i : temp){
+        for(int i : count){
             if(i != 0) return false;
         }
         return true;
