4c36bc7ac6ecc2b7b68477bcb8aae3745241abea
Jay He <heyingjie1993@outlook.com>
2016-04-09 10:59:28 -0700

    Leetcode

--- 21-Merge-Two-Sorted-Lists/solution.java
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -8,10 +8,8 @@
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
-        ListNode head = new ListNode(0);
-        ListNode p = head;
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
         while(l1 != null && l2 != null){
             if(l1.val < l2.val){
                 head.next = l1;
@@ -24,7 +22,7 @@ public class Solution {
             head = head.next;
         }
         if(l1 != null) head.next = l1;
-        if(l2 != null) head.next = l2;
-        return p.next;
+        else head.next = l2;
+        return dummy.next;
     }
 }
