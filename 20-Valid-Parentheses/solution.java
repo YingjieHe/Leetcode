db0fefcba3c08e6779037e68a3c2bd41c1f97a45
Jay He <heyingjie1993@outlook.com>
2016-04-10 22:02:21 -0700

    Leetcode

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -4,11 +4,11 @@ public class Solution {
         Stack<Character> stack = new Stack<Character>();
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            if(c == '{' || c == '(' || c == '[') stack.push(c);
-            else if(stack.isEmpty()) return false;
+            if(c == '(' || c == '{' || c == '[') stack.push(c);
+            else if(stack.isEmpty()) return false; 
             else if(c == ')' && stack.pop() != '(') return false;
-            else if(c == '}' && stack.pop() != '{') return false;
             else if(c == ']' && stack.pop() != '[') return false;
+            else if(c == '}' && stack.pop() != '{') return false;
         }
         return stack.isEmpty();
     }
