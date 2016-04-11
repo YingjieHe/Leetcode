b2577f17a4d321568a3beef1d1cc8ff0f28c4594
Jay He <heyingjie1993@outlook.com>
2016-04-11 16:43:01 -0700

    Leetcode

---
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null) return res;
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while(cur != null || !s.isEmpty()){
+            while(cur != null){
+                s.push(cur);
+                cur = cur.left;
+            }
+            cur = s.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+    }
+}
