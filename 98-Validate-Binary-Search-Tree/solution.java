e7d63a9a64ac6d0cf541b15862daa6004d9ccd39
Jay He <heyingjie1993@outlook.com>
2016-04-11 16:52:02 -0700

    Leetcode

---
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isvalid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean isvalid(TreeNode root, long min, long max){
+        if(root == null) return true;
+        if(root.val >= max || root.val <= min) return false;
+        return isvalid(root.left, min, root.val) && isvalid(root.right, root.val, max);
+    }
+}
