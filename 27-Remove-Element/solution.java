dca44a72db1b58c366ce27aa90daf9e46b7df933
Jay He <heyingjie1993@outlook.com>
2016-04-10 21:47:27 -0700

    Leetcode

--- 27-Remove-Element/solution.java
+++ 27-Remove-Element/solution.java
@@ -1,16 +1,12 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
         if(nums == null || nums.length == 0) return 0;
-        int n = nums.length - 1;
-        for(int i = 0; i <= n; i++){
-            if(nums[i] == val) {
-                int temp = nums[i];
-                nums[i] = nums[n];
-                nums[n] = temp;
-                i--;
-                n--;
+        int left = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[left++] = nums[i];
             }
         }
-        return n + 1;
+        return left;
     }
 }
