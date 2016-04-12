507b3708fe97d2916b11f21b1f54681298057d49
Jay He <heyingjie1993@outlook.com>
2016-04-11 19:36:56 -0700

    Leetcode

---
+++ 274-H-Index/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) return 0;
+        int n = citations.length;
+        int[] count = new int[n + 1];
+        for(int i = 0; i < n; i++){
+            if(citations[i] > n) count[n]++;
+            else count[citations[i]]++;
+        }
+        int sum = 0;
+        for(int i = n; i >= 0; i--){
+            sum += count[i];
+            if(sum >= i) return i;
+        }
+        return 0;
+    }
+}
