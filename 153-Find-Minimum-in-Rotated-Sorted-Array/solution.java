f7b7db0a43c128d664662172a272ea31ae51cfda
Jay He <heyingjie1993@outlook.com>
2016-04-11 23:04:13 -0700

    Leetcode

--- 153-Find-Minimum-in-Rotated-Sorted-Array/solution.java
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public int findMin(int[] nums) {
-        if(nums == null || nums.length == 0) return -1;
-        int left = 0, right = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
         while(left < right){
             int mid = left + (right - left) / 2;
-            if(nums[mid] > nums[right]) left = mid + 1;
-            else right = mid;
+            if(nums[mid] < nums[right]) right = mid;
+            else left = mid + 1;
         }
         return nums[left];
     }
