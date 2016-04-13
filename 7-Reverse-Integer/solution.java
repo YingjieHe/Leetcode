d17001901f16e30d0fb87221e6038544147f8450
Jay He <heyingjie1993@outlook.com>
2016-04-12 21:56:01 -0700

    Leetcode

--- 7-Reverse-Integer/solution.java
+++ 7-Reverse-Integer/solution.java
@@ -1,15 +1,11 @@
 public class Solution {
     public int reverse(int x) {
-        int temp = Math.abs(x);
-        int n = 0;
-        while(temp > 0){
-            if(n > Integer.MAX_VALUE / 10){  
-                return 0;  
-            }  
-            n = n * 10 + temp % 10;
-            temp /= 10;
+        long res = 0;
+        while(x != 0){
+            res = res * 10 + x % 10;
+            x /= 10;
+            if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
         }
-        if(x > 0) return n;
-        else return -n;
+        return (int)res;
     }
 }
