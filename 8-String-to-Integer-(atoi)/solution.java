2dd0b3707b6ab29e12f7eb0acd0b65eb1aa62378
Jay He <heyingjie1993@outlook.com>
2016-04-12 22:11:17 -0700

    Leetcode

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -1,30 +1,22 @@
 public class Solution {
     public int myAtoi(String str) {
-        if(str == null || str.length() == 0){
-            return 0;
-        }
+        if(str.length() == 0) return 0;
         str = str.trim();
-        int flag = 1;
         int i = 0;
+        int flag = 1;
         if(str.charAt(i) == '-'){
+            i++;
             flag = 0;
-            i++;
         }
-        else if(str.charAt(i) == '+'){
-            i++;
-        }
-        double n = 0;
+        else if(str.charAt(i) == '+') i++;
+        double res = 0;
         for(; i < str.length(); i++){
-            if(str.charAt(i) <= '9' && str.charAt(i) >= '0'){
-                n = n * 10 + (str.charAt(i) - '0');
-            }
+            if(str.charAt(i) >= '0' && str.charAt(i) <= '9') res = res * 10 + str.charAt(i) - '0';
             else break;
         }
-        if(flag == 0){
-            n = -n;
-        }
-        if(n > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-        if(n < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-        return (int)n;
+        if(flag == 0) res = -res;
+        if(res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(res < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        return (int)res;
     }
 }
