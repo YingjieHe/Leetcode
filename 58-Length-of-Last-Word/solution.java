f43cfda9642c3666c622c69c5430cd8f71eb23ee
Jay He <heyingjie1993@outlook.com>
2016-04-13 20:01:38 -0700

    Leetcode

--- 58-Length-of-Last-Word/solution.java
+++ 58-Length-of-Last-Word/solution.java
@@ -1,11 +1,12 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
-        String [] string = s.split(" ");
-        for (int i = string.length-1;i>=0;i--){
-            if(string[i].length()>0){
-                return string[i].length();
-            }
+        if(s == null || s.length() == 0) return 0;
+        int len = s.length() - 1, res = 0;
+        while(len >= 0 && s.charAt(len) == ' ') len--;
+        while(len >= 0 && s.charAt(len) != ' ') {
+            res++;
+            len--;
         }
-        return 0;
+        return res;
     }
 }
