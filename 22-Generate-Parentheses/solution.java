3d14e45237fa96ce1e830f7e10b1ebb245dc9ef0
Jay He <heyingjie1993@outlook.com>
2016-04-17 16:16:25 -0700

    Leetcode

--- 22-Generate-Parentheses/solution.java
+++ 22-Generate-Parentheses/solution.java
@@ -1,16 +1,17 @@
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<String>();
-        dfs(res, "", n, n);
+        if(n <= 0) return res;
+        generate(res, "", n, n);
         return res;
     }
-    private void dfs(List<String> res, String s, int left, int right){
+    public void generate(List<String> res, String s, int left, int right){
         if(left > right) return;
-        if(left == 0 && right == 0){
+        if(left == 0 && right == 0) {
             res.add(s);
             return;
         }
-        if(left > 0) dfs(res, s + "(", left - 1, right);
-        if(right > 0) dfs(res, s + ")", left, right - 1);
+        if(left > 0) generate(res, s + "(", left - 1, right);
+        if(right > 0) generate(res, s + ")", left, right - 1);
     }
 }
