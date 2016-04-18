2a1e5c22d34906fbeb38da933663f9e1c7761e2f
Jay He <heyingjie1993@outlook.com>
2016-04-17 21:27:19 -0700

    Leetcode

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,26 +1,26 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0) return res;
         Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++){
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j < k){
-                List<Integer> list = new ArrayList<Integer>();
-                if(nums[i] + nums[j] + nums[k] == 0){
+            int k = i + 1;
+            int m = nums.length - 1;
+            while(k < m){
+                int sum = nums[i] + nums[k] + nums[m];
+                if(sum == 0){
+                    List<Integer> list = new ArrayList<Integer>();
                     list.add(nums[i]);
-                    list.add(nums[j]);
                     list.add(nums[k]);
-                    j++;
-                    k--;
+                    list.add(nums[m]);
                     res.add(new ArrayList<Integer>(list));
-                    while(j < k && nums[k] == nums[k + 1]) k--;
-                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    k++;
+                    m--;
+                    while(k < m && nums[m] == nums[m + 1]) m--;
+                    while(k < m && nums[k] == nums[k - 1]) k++;
                 }
-                else if(nums[i] + nums[j] + nums[k] > 0){
-                    k--;
-                }
-                else j++;
+                else if(sum < 0) k++;
+                else m--;
             }
             while(i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
         }
