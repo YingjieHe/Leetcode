f80a8a3341944d2fcb0758d579ee011bb47f3e38
Jay He <heyingjie1993@outlook.com>
2016-04-18 14:56:37 -0700

    Leetcode

---
+++ 162-Find-Peak-Element/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums == null || nums.length == 0) return Integer.MAX_VALUE;
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int mid1 = left + (right - left) / 2;
+            int mid2 = mid1 + 1;
+            if(nums[mid1] < nums[mid2]) left = mid1 + 1;
+            else right = mid1;
+        }
+        return left;
+    }
+}
