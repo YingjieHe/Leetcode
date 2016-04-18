452d7016b2d0417c34253cd57b1ca693c6a5a1e9
Jay He <heyingjie1993@outlook.com>
2016-04-17 21:25:43 -0700

    Leetcode

--- 18-4Sum/solution.java
+++ 18-4Sum/solution.java
@@ -1,7 +1,8 @@
 public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
                 int k = j + 1;
@@ -14,14 +15,14 @@ public class Solution {
                         list.add(nums[j]);
                         list.add(nums[k]);
                         list.add(nums[m]);
-                        res.add(list);
-                        m--;
+                        res.add(new ArrayList<Integer>(list));
                         k++;
+                        m--;
                         while(k < m && nums[m] == nums[m + 1]) m--;
                         while(k < m && nums[k] == nums[k - 1]) k++;
                     }
-                    else if(sum > target) m--;
-                    else k++;
+                    else if(sum < target) k++;
+                    else m--;
                 }
                 while(j + 1 < nums.length && nums[j] == nums[j + 1]) j++;
             }
