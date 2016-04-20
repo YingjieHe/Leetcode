a72550abc4d43b01fd35d347364b087878cd0c4c
Jay He <heyingjie1993@outlook.com>
2016-04-19 18:19:33 -0700

    Leetcode

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,24 +1,23 @@
 public class Solution {
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
-        int res = 0;
+        int count = 0;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
-                    res++;
-                    merge(grid, i, j);
+                    count++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return res;
+        return count;
     }
-    public void merge(char[][] grid, int i, int j){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
-            return;
+    public void dfs(char[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
         grid[i][j] = '0';
-        merge(grid, i - 1, j);
-        merge(grid, i, j - 1);
-        merge(grid, i + 1, j);
-        merge(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 }
