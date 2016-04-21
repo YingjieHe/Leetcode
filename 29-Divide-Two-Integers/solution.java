a96a3b06be87a8d62630cdf7f98b2e25043d796e
Jay He <heyingjie1993@outlook.com>
2016-04-20 19:50:56 -0700

    Leetcode

--- 29-Divide-Two-Integers/solution.java
+++ 29-Divide-Two-Integers/solution.java
@@ -1,21 +1,17 @@
 public class Solution {
     public int divide(int dividend, int divisor) {
-        if(divisor == 0) return Integer.MAX_VALUE;
-        if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        if(divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1)) return Integer.MAX_VALUE;
         long dend = Math.abs((long)dividend);
         long sor = Math.abs((long)divisor);
-        int result = 0;
-        while(dend >= sor){
+        int res = 0;
+        while(sor <= dend){
             int offset = 0;
-            while(sor << offset <= dend) offset++;
-            result += 1 << (offset - 1);
+            while((sor << offset) <= dend) offset++;
             dend -= sor << (offset - 1);
+            res += 1 << (offset - 1);
         }
-        if((dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0)){
-            return result;
-        }
-        else{
-            return -result;
-        }
+        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+            return -res;
+        return res;
     }
 }
