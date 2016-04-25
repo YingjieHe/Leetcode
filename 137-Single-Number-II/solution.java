62a302a3ef5bf57357c1d0dc96e84ef7573acedd
Jay He <heyingjie1993@outlook.com>
2016-04-24 22:50:54 -0700

    Leetcode

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -1,12 +1,14 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        int counterOne = 0;
-        int counterTwo = 0;
-
-        for (int i = 0; i < nums.length; i++){
-            counterOne = (~counterTwo) & (counterOne ^ nums[i]);
-            counterTwo = (~counterOne) & (counterTwo ^ nums[i]);
+        int res = 0;
+        int count = 0;
+        for(int i = 0; i < 32; i++){
+            for(int j : nums){
+                count += (j >> i) & 1;
+            }
+            res += (count % 3) << i;
+            count = 0;
         }
-        return counterOne;
+        return res;
     }
 }
