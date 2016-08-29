700267bcb707a22f9563e788d0d0da9208396d0a
Jack He <heyingjie1993@outlook.com>
2016-08-29 05:21:08 -0700

    Leetcode

--- 155-Min-Stack/solution.java
+++ 155-Min-Stack/solution.java
@@ -1,28 +1,44 @@
-class MinStack {
-    class node{
+public class MinStack {
+
+    class node {
+        int value;
         int min;
-        int val;
-        public node(int x){
-            min = x;
-            val = x;
+        public node(int value) {
+            this.value = value;
+            min = value;
         }
     }
-    Stack<node> s = new Stack<node>();
+    
+    Stack<node> s;
+    /** initialize your data structure here. */
+    public MinStack() {
+        s = new Stack<>();
+    }
+    
     public void push(int x) {
         node n = new node(x);
-        if(!s.isEmpty()) n.min = Math.min(s.peek().min, x);
+        if(!s.isEmpty()) n.min = Math.min(x, s.peek().min);
         s.push(n);
     }
-
+    
     public void pop() {
         s.pop();
     }
-
+    
     public int top() {
-        return s.peek().val;
+        return s.peek().value;
     }
-
+    
     public int getMin() {
         return s.peek().min;
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
