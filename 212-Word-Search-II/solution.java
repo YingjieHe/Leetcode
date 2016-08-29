c0141281ded058c73e6020bc4304701136c2d466
Jack He <heyingjie1993@outlook.com>
2016-08-28 23:08:12 -0700

    Leetcode

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -1,31 +1,45 @@
 public class Solution {
-    Set<String> res = new HashSet<String>();
     public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = new Trie();
-        for(String s : words) {
-            trie.insert(s);
-        }
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
-                dfs(board, "", i, j, trie);
+                dfs(board, i, j, root, res);
             }
         }
-        return new ArrayList<String>(res);
+        return res;
     }
     
-    private void dfs(char[][] board, String s, int i, int j, Trie trie) {
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> res) {
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
-        if(board[i][j] == '*') return;
-        s += board[i][j];
-        if(!trie.startsWith(s)) return;
-        if(trie.search(s)) res.add(s);
-        char temp = board[i][j];
+        char c = board[i][j];
+        if(c == '*' || node.children[c - 'a'] == null) return;
+        node = node.children[c - 'a'];
+        if(node.word != null) {
+            res.add(node.word);
+            node.word = null;
+        }
         board[i][j] = '*';
-        dfs(board, s, i - 1, j, trie);
-        dfs(board, s, i, j - 1, trie);
-        dfs(board, s, i + 1, j, trie);
-        dfs(board, s, i, j + 1, trie);
-        board[i][j] = temp;
+        dfs(board, i - 1, j, node, res);
+        dfs(board, i, j - 1, node, res);
+        dfs(board, i + 1, j, node, res);
+        dfs(board, i, j + 1, node, res);
+        board[i][j] = c;
+    }
+    
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for(String word : words) {
+            TrieNode node = root;
+            for(char c : word.toCharArray()) {
+                if(node.children[c - 'a'] == null) {
+                    node.children[c - 'a'] = new TrieNode();
+                }
+                node = node.children[c - 'a'];
+            }
+            node.word = word;
+        }
+        return root;
     }
     
     class TrieNode {
@@ -33,47 +47,6 @@ public class Solution {
         String word;
         public TrieNode() {
             children = new TrieNode[26];
-            word = "";
-        }
-    }
-    
-    class Trie {
-        private TrieNode root;
-        public Trie() {
-            root = new TrieNode();
-        }
-        
-        public void insert(String s) {
-            TrieNode node = root;
-            for(char c : s.toCharArray()) {
-                if(node.children[c -'a'] == null) {
-                    node.children[c -'a'] = new TrieNode();
-                }
-                node = node.children[c -'a'];
-            }
-            node.word = s;
-        }
-        
-        public boolean search(String s) {
-            TrieNode node = root;
-            for(char c : s.toCharArray()) {
-                if(node.children[c -'a'] == null) {
-                    return false;
-                }
-                node = node.children[c -'a'];
-            }
-            return node.word.equals(s);
-        }
-        
-        public boolean startsWith(String s) {
-            TrieNode node = root;
-            for(char c : s.toCharArray()) {
-                if(node.children[c -'a'] == null) {
-                    return false;
-                }
-                node = node.children[c -'a'];
-            }
-            return true;
         }
     }
 }
