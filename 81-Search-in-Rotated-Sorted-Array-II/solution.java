97473584fe622ed712d117f152e8f04753c20e3c
Jack He <heyingjie1993@outlook.com>
2016-08-31 00:16:57 -0700

    Leetcode

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -3,15 +3,15 @@ public class Solution {
         if(nums == null || nums.length == 0) return false;
         int left = 0;
         int right = nums.length - 1;
-        while(left <= right){
+        while(left <= right) {
             int mid = left + (right - left) / 2;
             if(nums[mid] == target) return true;
-            else if(nums[mid] > nums[left]){
+            if(nums[left] < nums[mid]) {
                 if(nums[left] <= target && target < nums[mid]) right = mid - 1;
                 else left = mid + 1;
             }
-            else if(nums[mid] == nums[left]) left++;
-            else{
+            else if(nums[left] == nums[mid]) left++;
+            else {
                 if(nums[right] >= target && target > nums[mid]) left = mid + 1;
                 else right = mid - 1;
             }
