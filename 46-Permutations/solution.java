2350203411f269e970f5131c3849783f8d7b8af8
Jack He <heyingjie1993@outlook.com>
2016-08-31 02:45:01 -0700

    Leetcode

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new ArrayList<>();
         if(nums == null || nums.length == 0) return res;
-        List<Integer> list = new ArrayList<Integer>();
-        dfs(res, list, nums);
+        dfs(res, nums, new ArrayList<Integer>());
         return res;
     }
-    public void dfs(List<List<Integer>> res, List<Integer> list, int[] nums){
-        if(list.size() == nums.length){
+    
+    private void dfs(List<List<Integer>> res, int[] nums, List<Integer> list) {
+        if(list.size() == nums.length) {
             res.add(new ArrayList<Integer>(list));
             return;
         }
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++) {
             if(list.contains(nums[i])) continue;
             list.add(nums[i]);
-            dfs(res, list, nums);
+            dfs(res, nums, list);
             list.remove(list.size() - 1);
         }
     }
