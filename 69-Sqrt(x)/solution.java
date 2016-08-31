e3426690f752f460f9960681604dcced90e045a0
Jack He <heyingjie1993@outlook.com>
2016-08-31 01:20:04 -0700

    Leetcode

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -1,9 +1,13 @@
 public class Solution {
     public int mySqrt(int x) {
-        long r = x;
-        while(r * r > x) {
-            r = (r + x / r) / 2;
+        if(x == 0 || x == 1) return x;
+        int left = 1;
+        int right = x;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(mid <= x / mid) left = mid + 1;
+            else right = mid;
         }
-        return (int)r;
+        return left - 1;
     }
 }
