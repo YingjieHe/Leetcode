1c87510b69a82b33b92200d13956b1ca9f93cae9
Jack He <heyingjie1993@outlook.com>
2016-09-01 01:08:33 -0700

    Leetcode

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -9,11 +9,26 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return root == null || dfs(root.left, root.right);
-    }
-    public boolean dfs(TreeNode left, TreeNode right){
-        if(left == null || right == null) return left == right;
-        if(right.val != left.val) return false;
-        return dfs(right.right, left.left) && dfs(left.right, right.left);
+        if(root == null || (root.left == null && root.right == null)) return true;
+        if(root.left == null || root.right == null) return false;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+        while(!q.isEmpty()) {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if(left.val != right.val) return false;
+            if(left.left != null && right.right != null) {
+                q.offer(left.left);
+                q.offer(right.right);
+            }
+            else if(left.left == null ^ right.right == null) return false;
+            if(left.right != null && right.left != null) {
+                q.offer(left.right);
+                q.offer(right.left);
+            }
+            else if(left.right == null ^ right.left == null) return false;
+        }
+        return true;
     }
 }
