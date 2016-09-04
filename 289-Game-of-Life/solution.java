93924501e3b0320b74b6fe1cdb52d70dfbd68ce8
Jack He <heyingjie1993@outlook.com>
2016-09-03 23:16:02 -0700

    Leetcode

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -1,21 +1,23 @@
 public class Solution {
-    int[][] dir ={{1,-1},{1,0},{1,1},{0,-1},{0,1},{-1,-1},{-1,0},{-1,1}};
+    int[][] dirs ={{1,-1},{1,0},{1,1},{0,-1},{0,1},{-1,-1},{-1,0},{-1,1}};
     public void gameOfLife(int[][] board) {
         if(board == null || board.length == 0 || board[0].length == 0) return;
-        int n = board.length, m = board[0].length;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 int live = 0;
-                for(int[] d : dir){
-                    if(d[0] + i < 0 || d[0] + i >= n || d[1] + j < 0 || d[1] + j >= m) continue;
-                    live += board[d[0] + i][d[1] + j] & 1;
+                for(int[] dir : dirs) {
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+                    if(x >= 0 && x < m && y >= 0 && y < n && (board[x][y] & 1) == 1) live++;
                 }
                 if(board[i][j] == 1 && live >= 2 && live <= 3) board[i][j] = 3;
                 if(board[i][j] == 0 && live == 3) board[i][j] = 2;
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 board[i][j] >>= 1;
             }
         }
