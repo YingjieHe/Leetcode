831477e28c2bc27dad53130a22289d7a06bb75a0
Jack He <heyingjie1993@outlook.com>
2016-09-05 06:50:10 -0700

    Leetcode

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -6,20 +6,19 @@ public class Solution {
             for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                 }
             }
         }
         return count;
     }
     
-    private void dfs(char[][] grid, int i, int j) {
-        if(i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1) return;
-        if(grid[i][j] == '0') return;
+    private void bfs(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
         grid[i][j] = '0';
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j - 1);
-        dfs(grid, i, j + 1);
+        bfs(grid, i - 1, j);
+        bfs(grid, i + 1, j);
+        bfs(grid, i, j - 1);
+        bfs(grid, i, j + 1);
     }
 }
