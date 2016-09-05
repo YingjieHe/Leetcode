3d314488d45b204dc6cbe7c7ddb1c680ba65d346
Jack He <heyingjie1993@outlook.com>
2016-09-04 23:21:25 -0700

    Leetcode

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -8,27 +8,26 @@
  */
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null || lists.length == 0) {
-            return null;
-        }
+        if(lists == null || lists.length == 0) return null;
         PriorityQueue<ListNode> q = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>(){
             public int compare(ListNode a, ListNode b) {
                 return a.val - b.val;
             }
         });
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
-        for(ListNode node : lists) {
-            if(node != null) {
-                q.offer(node);
+        
+        for(ListNode n : lists) {
+            if(n != null) {
+                q.offer(n);
             }
         }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
         while(!q.isEmpty()) {
-            head.next = q.poll();
+            ListNode temp = q.poll();
+            head.next = temp;
+            if(temp.next != null) q.offer(temp.next);
             head = head.next;
-            if(head.next != null) {
-                q.offer(head.next);
-            }
         }
         return dummy.next;
     }
