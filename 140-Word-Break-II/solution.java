c76e38696e5dd448bcad341a0115855c7bba2885
Jack He <heyingjie1993@outlook.com>
2016-09-04 22:35:07 -0700

    Leetcode

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,13 +1,10 @@
 public class Solution {
     public List<String> wordBreak(String s, Set<String> wordDict) {
-        return dfs(s, wordDict, new HashMap<String, List<String>>());
+        return dfs(s, wordDict,  new HashMap<String, List<String>>());
     }
     
     private List<String> dfs(String s, Set<String> wordDict, Map<String, List<String>> map) {
-        if(map.containsKey(s)) {
-            return map.get(s);
-        }
-        
+        if(map.containsKey(s)) return map.get(s);
         List<String> list = new ArrayList<String>();
         if(s.length() == 0) {
             list.add("");
@@ -16,12 +13,11 @@ public class Solution {
         for(String word : wordDict) {
             if(s.startsWith(word)) {
                 List<String> sublist = dfs(s.substring(word.length()), wordDict, map);
-                for(String sub : sublist) {
-                    list.add(word + (sub.isEmpty()? "": " ") + sub);
+                for(String substring : sublist) {
+                    list.add(word + (substring.isEmpty()? "": " ") + substring);
                 }
             }
         }
-        
         map.put(s, list);
         return list;
     }
