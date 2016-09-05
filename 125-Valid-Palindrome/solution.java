e0aeaa38ff8e545ed2e45897c5927f2d57bb2586
Jack He <heyingjie1993@outlook.com>
2016-09-05 01:13:59 -0700

    Leetcode

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -1,17 +1,13 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        if(s == null || s.length() <= 1) return true;
+        if(s == null || s.length() == 0) return true;
         int left = 0;
         int right = s.length() - 1;
         while(left <= right) {
             char head = s.charAt(left);
             char tail = s.charAt(right);
-            if(!Character.isLetterOrDigit(head)) {
-                left++;
-            }
-            else if(!Character.isLetterOrDigit(tail)) {
-                right--;
-            }
+            if(!Character.isLetterOrDigit(head)) left++;
+            else if(!Character.isLetterOrDigit(tail)) right--;
             else {
                 if(Character.toLowerCase(head) != Character.toLowerCase(tail)) return false;
                 left++;
