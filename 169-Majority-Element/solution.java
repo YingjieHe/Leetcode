30fa702593c6a32ff5ca609023b1f569146e5a72
Jack He <heyingjie1993@outlook.com>
2016-09-04 23:34:45 -0700

    Leetcode

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -2,14 +2,13 @@ public class Solution {
     public int majorityElement(int[] nums) {
         int res = nums[0];
         int count = 1;
-        for(int i = 1; i < nums.length; i++){
+        for(int i = 1; i < nums.length; i++) {
             if(count == 0) {
-                res = nums[i];
                 count++;
+                res = nums[i];
             }
-            else if(nums[i] == res) count++;
+            else if(res == nums[i]) count++;
             else count--;
-            
         }
         return res;
     }
