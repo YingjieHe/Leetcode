8d272d5789be64e3e03f861d1384ba01dd438e37
Jack He <heyingjie1993@outlook.com>
2016-09-07 22:24:27 -0700

    Leetcode

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,11 +1,14 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> seen = new HashSet<>();
-        Set<String> res = new HashSet<>();
+        if(s == null || s.length() <= 10) return new ArrayList<String>();
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
         for(int i = 0; i + 9 < s.length(); i++) {
             String str = s.substring(i, i + 10);
-            if(!seen.add(str)) res.add(str);
+            if(!set1.add(str)) {
+                set2.add(str);
+            }
         }
-        return new ArrayList<String>(res);
+        return new ArrayList<String>(set2);
     }
 }
