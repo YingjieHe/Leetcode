bbd092acd07a12f23e58d3b0861256ca339f4452
Jack He <heyingjie1993@outlook.com>
2016-09-08 00:13:49 -0700

    Leetcode

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,12 +1,13 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int[] count = new int[256];
-        for(int i = 0; i < s.length(); i++){
-            count[s.charAt(i)]++;
-            count[t.charAt(i)]--;
+        if(s == null && t == null) return true;
+        if(s == null || t == null || s.length() != t.length()) return false;
+        int[] arr = new int[256];
+        for(int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i)]++;
+            arr[t.charAt(i)]--;
         }
-        for(int i : count){
+        for(int i : arr) {
             if(i != 0) return false;
         }
         return true;
