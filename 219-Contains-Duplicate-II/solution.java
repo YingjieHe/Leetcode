860bfe9bd3b0182b54ddd20e9d069c73f63fc908
Jack He <heyingjie1993@outlook.com>
2016-09-13 11:11:04 -0700

    Leetcode

--- 219-Contains-Duplicate-II/solution.java
+++ 219-Contains-Duplicate-II/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums == null || nums.length <= 1) return false;
         Set<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.length; i++) {
             if(i > k) set.remove(nums[i - k - 1]);
