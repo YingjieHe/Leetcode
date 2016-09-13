141edb8af1532691de0f59748e03f8eb3da7c26c
Jack He <heyingjie1993@outlook.com>
2016-09-12 22:21:34 -0700

    Leetcode

--- 5-Longest-Palindromic-Substring/solution.java
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -2,14 +2,12 @@ public class Solution {
     public String longestPalindrome(String s) {
         if(s == null || s.length() <= 1) return s;
         int len = s.length();
-        boolean[][] dp = new boolean[len][len];
+        boolean[][] dp =  new boolean[len][len];
         String res = null;
         for(int i = len - 1; i >= 0; i--) {
             for(int j = i; j < len; j++) {
                 dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
-                if(dp[i][j] && (res == null || j - i + 1 > res.length())) {
-                    res = s.substring(i, j + 1);
-                }
+                if(dp[i][j] && (res == null || j - i + 1 > res.length())) res = s.substring(i, j + 1);
             }
         }
         return res;
