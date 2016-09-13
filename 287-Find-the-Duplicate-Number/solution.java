ab4712534277d0354d21acaad8cdcdec8343718b
Jack He <heyingjie1993@outlook.com>
2016-09-12 23:02:56 -0700

    Leetcode

--- 287-Find-the-Duplicate-Number/solution.java
+++ 287-Find-the-Duplicate-Number/solution.java
@@ -1,6 +1,5 @@
 public class Solution {
     public int findDuplicate(int[] nums) {
-        if(nums == null || nums.length <= 1) return -1;
         int slow = nums[0];
         int fast = nums[nums[0]];
         while(slow != fast) {
@@ -9,8 +8,8 @@ public class Solution {
         }
         fast = 0;
         while(slow != fast) {
-            fast = nums[fast];
             slow = nums[slow];
+            fast = nums[fast];
         }
         return slow;
     }
