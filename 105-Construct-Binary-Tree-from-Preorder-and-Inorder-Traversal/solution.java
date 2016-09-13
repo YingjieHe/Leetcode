f31bb058e2c04b5dc881706bd17e829b47832d89
Jack He <heyingjie1993@outlook.com>
2016-09-12 21:32:28 -0700

    Leetcode

--- 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
@@ -12,18 +12,15 @@ public class Solution {
         return dfs(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
     
-    private TreeNode dfs(int[] preorder, int ps, int pe, int[] inorder, int is, int ie) {
+    public TreeNode dfs(int[] preorder, int ps, int pe, int[] inorder, int is, int ie) {
         if(ps > pe || is > ie) return null;
         TreeNode root = new TreeNode(preorder[ps]);
-        int index = 0;
-        for(int i = is; i <= ie; i++) {
-            if(preorder[ps] == inorder[i]) {
-                index = i;
-                break;
-            }
+        int i = is;
+        for(; i <= ie; i++) {
+            if(inorder[i] == preorder[ps]) break;
         }
-        root.left = dfs(preorder, ps + 1, ps + index - is, inorder, is, index - 1);
-        root.right = dfs(preorder, ps + index - is + 1, pe, inorder, index + 1, ie);
+        root.left = dfs(preorder, ps + 1, ps + i - is, inorder, is, i - 1);
+        root.right = dfs(preorder, ps + i - is + 1, pe, inorder, i + 1, ie);
         return root;
     }
 }
