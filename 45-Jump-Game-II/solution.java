d3c50d96b6ec1136c74889d4c8cd4662ccb6188d
Jack He <heyingjie1993@outlook.com>
2016-09-14 15:02:14 -0700

    Leetcode

---
+++ 45-Jump-Game-II/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int jump(int[] nums) {
+        if(nums == null || nums.length < 2) return 0;
+        int currentMax = 0;
+        int nextMax = 0;
+        int count = 0;
+        int i = 0;
+        while(currentMax >= i) {
+            count++;
+            for(; i <= currentMax; i++) {
+                nextMax = Math.max(nextMax, nums[i] + i);
+                if(nextMax >= nums.length - 1) return count;
+            }
+            currentMax = nextMax;
+        }
+        return Integer.MAX_VALUE;
+    }
+}
