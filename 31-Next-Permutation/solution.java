c50fa3ee655167e35d82483e79fdbbfb206113b1
Jack He <heyingjie1993@outlook.com>
2016-09-14 14:46:52 -0700

    Leetcode

--- 31-Next-Permutation/solution.java
+++ 31-Next-Permutation/solution.java
@@ -2,33 +2,33 @@ public class Solution {
     public void nextPermutation(int[] nums) {
         if(nums == null || nums.length <= 1) return;
         int i = nums.length - 1;
-        for(;i > 0; i--) {
+        for(; i > 0; i--) {
             if(nums[i] > nums[i - 1]) break;
         }
         if(i != 0) swap(nums, i - 1);
         reverse(nums, i);
     }
     
-    private void swap(int[] nums, int i) {
-        for(int k = nums.length - 1; k > i; k--) {
-            if(nums[k] > nums[i]) {
-                int temp = nums[k];
-                nums[k] = nums[i];
+    public void swap(int[] nums, int i) {
+        for(int j = nums.length - 1; j > i; j--) {
+            if(nums[j] > nums[i]) {
+                int temp = nums[j];
+                nums[j] = nums[i];
                 nums[i] = temp;
                 break;
             }
         }
     }
     
-    private void reverse(int[] nums, int i) {
+    public void reverse(int[] nums, int i) {
         int start = i;
         int end = nums.length - 1;
         while(start < end) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
-            start++;
             end--;
+            start++;
         }
     }
 }
