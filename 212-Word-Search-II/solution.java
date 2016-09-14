a682dd26364a52f9836ee1e48a3c55d9044534fc
Jack He <heyingjie1993@outlook.com>
2016-09-14 10:25:36 -0700

    Leetcode

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -1,7 +1,8 @@
 public class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> res = new ArrayList<>();
-        TrieNode root = buildTrie(words);
+        if(board == null || board.length == 0 || board[0].length == 0) return res;
+        TrieNode root = buildTree(words);
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
                 dfs(board, i, j, root, res);
@@ -10,42 +11,42 @@ public class Solution {
         return res;
     }
     
-    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> res) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+    private void dfs(char[][] board, int i, int j, TrieNode root, List<String> res) {
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) return;
         char c = board[i][j];
-        if(c == '*' || node.children[c - 'a'] == null) return;
-        node = node.children[c - 'a'];
-        if(node.word != null) {
-            res.add(node.word);
-            node.word = null;
+        if(c == '*' || root.children[c - 'a'] == null) return;
+        root = root.children[c - 'a'];
+        if(root.word != null) {
+            res.add(root.word);
+            root.word = null;
         }
         board[i][j] = '*';
-        dfs(board, i - 1, j, node, res);
-        dfs(board, i, j - 1, node, res);
-        dfs(board, i + 1, j, node, res);
-        dfs(board, i, j + 1, node, res);
+        dfs(board, i - 1, j, root, res);
+        dfs(board, i + 1, j, root, res);
+        dfs(board, i, j - 1, root, res);
+        dfs(board, i, j + 1, root, res);
         board[i][j] = c;
     }
     
-    private TrieNode buildTrie(String[] words) {
+    private TrieNode buildTree(String[] words) {
         TrieNode root = new TrieNode();
-        for(String word : words) {
+        for(String s : words) {
             TrieNode node = root;
-            for(char c : word.toCharArray()) {
+            for(char c : s.toCharArray()) {
                 if(node.children[c - 'a'] == null) {
                     node.children[c - 'a'] = new TrieNode();
                 }
                 node = node.children[c - 'a'];
             }
-            node.word = word;
+            node.word = s;
         }
         return root;
     }
     
-    class TrieNode {
-        TrieNode[] children;
+    class TrieNode{
         String word;
-        public TrieNode() {
+        TrieNode[] children;
+        public TrieNode(){
             children = new TrieNode[26];
         }
     }
