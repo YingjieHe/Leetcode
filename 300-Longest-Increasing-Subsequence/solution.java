71d882e3a2dd27298f02dc0e9d03222ee3b4a244
Jack He <heyingjie1993@outlook.com>
2016-09-15 13:35:04 -0700

    Leetcode

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -2,15 +2,15 @@ public class Solution {
     public int lengthOfLIS(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
         int[] dp = new int[nums.length];
+        int max = 0;
         Arrays.fill(dp, 1);
-        int max = Integer.MIN_VALUE;
         for(int i = 0; i < nums.length; i++) {
             for(int j = 0; j < i; j++) {
-                if(nums[i] > nums[j]) {
+                if(nums[j] < nums[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            max= Math.max(max, dp[i]);
+            max = Math.max(max, dp[i]);
         }
         return max;
     }
