2eccd2796acb2206c8caf4b49554d2b26d571051
Jack He <heyingjie1993@outlook.com>
2016-09-16 16:42:11 -0700

    Leetcode

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -3,33 +3,34 @@ public class Solution {
         if(s == null || s.length() < t.length() || s.length() == 0){
             return "";
         }
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
         for(int i = 0; i < t.length(); i++) {
             char c = t.charAt(i);
-            if(map.containsKey(c)) map.put(c, map.get(c) + 1);
-            else map.put(c, 1);
+            if(!map.containsKey(c)) map.put(c, 1);
+            else map.put(c, map.get(c) + 1);
         }
-        int start = 0, end = 0, count = t.length(), len = s.length() + 1, head = 0;
+        int start = 0, end = 0, count = t.length();
+        int minStart = 0, minLen = s.length() + 1;
         while(end < s.length()) {
-            char c = s.charAt(end);
-            if(map.containsKey(c)) {
-                map.put(c, map.get(c) - 1);
-                if(map.get(c) >= 0) count--;
+            char endChar = s.charAt(end);
+            if(map.containsKey(endChar)) {
+                map.put(endChar, map.get(endChar) - 1);
+                if(map.get(endChar) >= 0) count--;
             }
             while(count == 0) {
-                if(end - start + 1 < len) {
-                    len = end - start + 1;
-                    head = start;
+                if(end - start + 1 < minLen) {
+                    minLen = end - start + 1;
+                    minStart = start;
                 }
-                char ch = s.charAt(start);
-                if(map.containsKey(ch)) {
-                    map.put(ch, map.get(ch) + 1);
-                    if(map.get(ch) > 0) count++;
+                char startChar = s.charAt(start);
+                if(map.containsKey(startChar)) {
+                    map.put(startChar, map.get(startChar) + 1);
+                    if(map.get(startChar) > 0) count++;
                 }
                 start++;
             }
             end++;
         }
-        return len == s.length() + 1? "": s.substring(head, head + len);
+        return minLen == s.length() + 1? "" : s.substring(minStart, minStart + minLen);
     }
 }
