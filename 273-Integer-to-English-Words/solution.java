da3b08c66043a5c41404f11b484183a63163afdc
Jack He <heyingjie1993@outlook.com>
2016-09-16 16:03:10 -0700

    Leetcode

--- 273-Integer-to-English-Words/solution.java
+++ 273-Integer-to-English-Words/solution.java
@@ -4,11 +4,11 @@ public class Solution {
     private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
     public String numberToWords(int num) {
         if(num == 0) return "Zero";
-        String res = "";
         int i = 0;
+        String res = "";
         while(num != 0) {
             if(num % 1000 != 0) {
-                res = helper(num % 1000) + THOUSANDS[i] + " " + res;
+                res = help(num % 1000) + THOUSANDS[i] + " " + res;
             }
             num /= 1000;
             i++;
@@ -16,10 +16,10 @@ public class Solution {
         return res.trim();
     }
     
-    public String helper(int num) {
+    private String help(int num) {
         if(num == 0) return "";
         else if(num < 20) return LESS_THAN_20[num] + " ";
-        else if(num < 100) return TENS[num / 10] + " " + helper(num % 10);
-        else return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+        else if(num < 100) return TENS[num / 10] + " " + help(num % 10);
+        else return LESS_THAN_20[num / 100] + " Hundred " + help(num % 100);
     }
 }
