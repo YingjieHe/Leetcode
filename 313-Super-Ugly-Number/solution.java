eb291f9732052601f4dacdfd55db60f77ea8a730
Jack He <heyingjie1993@outlook.com>
2016-09-18 10:00:01 -0700

    Leetcode

--- 313-Super-Ugly-Number/solution.java
+++ 313-Super-Ugly-Number/solution.java
@@ -3,12 +3,12 @@ public class Solution {
         int[] res = new int[n];
         int[] id = new int[primes.length];
         res[0] = 1;
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < n; i++) {
             res[i] = Integer.MAX_VALUE;
-            for(int j = 0; j < primes.length; j++){
+            for(int j = 0; j < primes.length; j++) {
                 res[i] = Math.min(res[i], res[id[j]] * primes[j]);
             }
-            for(int j = 0; j < primes.length; j++){
+            for(int j = 0; j < primes.length; j++) {
                 if(res[i] == res[id[j]] * primes[j]) id[j]++;
             }
         }
