81c4953738f94ad464589f0e83ca209f7aceff18
Jack He <heyingjie1993@outlook.com>
2016-09-18 16:14:18 -0700

    Leetcode

--- 332-Reconstruct-Itinerary/solution.java
+++ 332-Reconstruct-Itinerary/solution.java
@@ -3,13 +3,16 @@ public class Solution {
         List<String> res = new LinkedList<>();
         if(tickets == null || tickets.length == 0 || tickets[0].length == 0) return res;
         Map<String, PriorityQueue<String>> map = new HashMap<>();
-        for(String[] ticket : tickets) {
-            if(!map.containsKey(ticket[0])) map.put(ticket[0], new PriorityQueue<String>());
-            map.get(ticket[0]).add(ticket[1]);
+        for(String[] s : tickets) {
+            if(!map.containsKey(s[0])) {
+                map.put(s[0], new PriorityQueue<String>());
+            }
+            map.get(s[0]).add(s[1]);
         }
         visit("JFK", res, map);
         return res;
     }
+    
     private void visit(String airport, List<String> res, Map<String, PriorityQueue<String>> map) {
         while(map.containsKey(airport) && map.get(airport).size() != 0) {
             visit(map.get(airport).poll(), res, map);
