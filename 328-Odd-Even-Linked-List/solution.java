f5b0ae0451db1a996f3d8fe8a93f24de93ab6663
Jack He <heyingjie1993@outlook.com>
2016-09-18 23:43:36 -0700

    Leetcode

--- 328-Odd-Even-Linked-List/solution.java
+++ 328-Odd-Even-Linked-List/solution.java
@@ -8,10 +8,10 @@
  */
 public class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head == null || head.next == null) return head;
+        if(head == null) return head;
         ListNode odd = head, even = head.next, evenHead = even;
         while(even != null && even.next != null) {
-            odd.next = odd.next.next;
+            odd.next =odd.next.next;
             even.next = even.next.next;
             odd = odd.next;
             even = even.next;
