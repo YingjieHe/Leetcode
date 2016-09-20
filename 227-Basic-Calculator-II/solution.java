b8ac00b74983868a246c2c5c459db008b1aa2589
Jack He <heyingjie1993@outlook.com>
2016-09-20 11:01:02 -0700

    Leetcode

--- 227-Basic-Calculator-II/solution.java
+++ 227-Basic-Calculator-II/solution.java
@@ -1,16 +1,32 @@
 public class Solution {
     public int calculate(String s) {
         if(s == null || s.length() == 0) return 0;
-        int len = s.length();
         Stack<Integer> stack = new Stack<>();
-        char op = '+';
+        int len = s.length();
         int num = 0;
+        char op = '+';
+        
         for(int i = 0; i < len; i++) {
             char c = s.charAt(i);
-            if(Character.isDigit(c)) {
-                num = num * 10 + c - '0';
+            if(c == '(') {
+                int start = i;
+                int count = 0;
+                do {
+                    c = s.charAt(i);
+                    if(c == '(') count++;
+                    else if(c == ')') count--;
+                    i++;
+                }
+                while(count != 0);
+                num = calculate(s.substring(start, i - 1));
             }
-            if((!Character.isDigit(c) && c != ' ') || i == len - 1) {
+            if(i == len) i--;
+            c = s.charAt(i);
+            if(Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            
+            if((!Character.isDigit(c) && c !=' ') || i == len - 1) {
                 if(op == '+') {
                     stack.push(num);
                 }
@@ -27,6 +43,7 @@ public class Solution {
                 num = 0;
             }
         }
+        
         int res = 0;
         for(int i : stack) {
             res += i;
