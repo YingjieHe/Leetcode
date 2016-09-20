2713cba21fe1efe627bfa889b48e3727834164f4
Jack He <heyingjie1993@outlook.com>
2016-09-20 11:57:49 -0700

    Leetcode

--- 210-Course-Schedule-II/solution.java
+++ 210-Course-Schedule-II/solution.java
@@ -2,15 +2,21 @@ public class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
         Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int[] arr : prerequisites) {
-            if(!map.containsKey(arr[1])) map.put(arr[1], new ArrayList<Integer>());
-            map.get(arr[1]).add(arr[0]);
-            indegree[arr[0]]++;
+        for(int[] prerequisite : prerequisites) {
+            indegree[prerequisite[0]]++;
+            if(!map.containsKey(prerequisite[1])) {
+                map.put(prerequisite[1], new ArrayList<Integer>());
+            }
+            map.get(prerequisite[1]).add(prerequisite[0]);
         }
+        
         Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) q.offer(i);
+            if(indegree[i] == 0) {
+                q.offer(i);
+            }
         }
+        
         int[] res = new int[numCourses];
         int order = 0;
         while(!q.isEmpty()) {
@@ -18,7 +24,8 @@ public class Solution {
             res[order++] = course;
             if(map.containsKey(course)) {
                 for(int i : map.get(course)) {
-                    if(--indegree[i] == 0) q.offer(i);
+                    indegree[i]--;
+                    if(indegree[i] == 0) q.offer(i);
                 }
             }
         }
