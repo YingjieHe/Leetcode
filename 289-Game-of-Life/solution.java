b364a03579bab8f36a33015299b8cfa1dcf7a175
Jack He <heyingjie1993@outlook.com>
2016-09-20 11:30:59 -0700

    Leetcode

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-    int[][] dirs ={{1,-1},{1,0},{1,1},{0,-1},{0,1},{-1,-1},{-1,0},{-1,1}};
+    int[][] dirs = {{1,-1},{1,0},{1,1},{0,-1},{0,1},{-1,-1},{-1,0},{-1,1}};
     public void gameOfLife(int[][] board) {
         if(board == null || board.length == 0 || board[0].length == 0) return;
         int m = board.length;
@@ -12,7 +12,7 @@ public class Solution {
                     int y = j + dir[1];
                     if(x >= 0 && x < m && y >= 0 && y < n && (board[x][y] & 1) == 1) live++;
                 }
-                if(board[i][j] == 1 && live >= 2 && live <= 3) board[i][j] = 3;
+                if(board[i][j] == 1 && (live == 2 || live == 3)) board[i][j] = 3;
                 if(board[i][j] == 0 && live == 3) board[i][j] = 2;
             }
         }
