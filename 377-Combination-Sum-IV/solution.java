634811203792d8ed842eb2f3c59bfbab7f583622
Jack He <heyingjie1993@outlook.com>
2016-09-20 21:08:09 -0700

    Leetcode

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -1,12 +1,11 @@
 public class Solution {
     public int combinationSum4(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
         int[] res = new int[target + 1];
         res[0] = 1;
-        for(int i = 0; i <= target; i++) {
-            for(int j = 0; j < nums.length; j++) {
-                if(i >= nums[j]) {
-                    res[i] += res[i - nums[j]];
-                }
+        for(int i = 1; i <= target; i++) {
+            for(int num : nums) {
+                if(num <= i) res[i] += res[i - num];
             }
         }
         return res[target];
