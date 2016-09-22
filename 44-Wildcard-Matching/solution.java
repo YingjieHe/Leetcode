eb0ab64ee26abd0ea4aabd9834bb9b38b0250fe5
Jack He <heyingjie1993@outlook.com>
2016-09-21 17:10:23 -0700

    Leetcode

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,25 +1,23 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int s1 = 0, p1 = 0, match = 0, starIdx = -1;            
-        while (s1 < s.length()){
-            if (p1 < p.length()  && (p.charAt(p1) == '?' || s.charAt(s1) == p.charAt(p1))){
-                s1++;
-                p1++;
+        int slen = 0, plen = 0, match = 0, startId = -1;
+        while(slen < s.length()) {
+            if(plen < p.length() && (p.charAt(plen) == '?' || p.charAt(plen) == s.charAt(slen))) {
+                slen++;
+                plen++;
             }
-            else if (p1 < p.length() && p.charAt(p1) == '*'){
-                starIdx = p1;
-                match = s1;
-                p1++;
+            else if(plen < p.length() && p.charAt(plen) == '*') {
+                match = slen;
+                startId = plen;
+                plen++;
             }
-            else if (starIdx != -1){
-                p1 = starIdx + 1;
-                match++;
-                s1 = match;
+            else if(startId != -1) {
+                slen = ++match;
+                plen = startId + 1;
             }
             else return false;
         }
-        while (p1 < p.length() && p.charAt(p1) == '*')
-            p1++;
-        return p1 == p.length();
+        while(plen < p.length() && p.charAt(plen) == '*') plen++;
+        return plen == p.length();
     }
 }
