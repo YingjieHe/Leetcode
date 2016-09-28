8f7ecdd8b3c9d2126824689adbfe4d7371d4f4e5
Jack He <heyingjie1993@outlook.com>
2016-09-27 20:57:02 -0700

    Leetcode

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int slen = 0, plen = 0, match = 0, startId = -1;
+        int slen = 0, plen = 0, match = 0, start = -1;
         while(slen < s.length()) {
-            if(plen < p.length() && (p.charAt(plen) == '?' || p.charAt(plen) == s.charAt(slen))) {
-                slen++;
+            if(plen < p.length() && (p.charAt(plen) == '?' || s.charAt(slen) == p.charAt(plen))) {
                 plen++;
+                slen++;
             }
             else if(plen < p.length() && p.charAt(plen) == '*') {
                 match = slen;
-                startId = plen;
-                plen++;
+                start = plen++;
             }
-            else if(startId != -1) {
+            else if(start != -1) {
                 slen = ++match;
-                plen = startId + 1;
+                plen = start + 1;
             }
             else return false;
         }
