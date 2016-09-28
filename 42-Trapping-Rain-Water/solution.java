126bc86cf6aa740c3aae60478e9f8a831b4b22fd
Jack He <heyingjie1993@outlook.com>
2016-09-27 21:40:21 -0700

    Leetcode

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -2,18 +2,15 @@ public class Solution {
     public int trap(int[] height) {
         if(height == null || height.length == 0) return 0;
         int left = 0, right = height.length - 1;
-        int max = 0, leftMax = 0, rightMax = 0;
+        int leftMax = Integer.MIN_VALUE, rightMax = Integer.MIN_VALUE;
+        int max = 0;
         while(left < right) {
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
-            if(leftMax <= rightMax) {
-                max += leftMax - height[left];
-                left++;
+            leftMax = Math.max(height[left], leftMax);
+            rightMax = Math.max(height[right], rightMax);
+            if(leftMax < rightMax) {
+                max += leftMax - height[left++];
             }
-            else {
-                max += rightMax - height[right];
-                right--;
-            }
+            else max += rightMax - height[right--];
         }
         return max;
     }
