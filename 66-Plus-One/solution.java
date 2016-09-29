a73c4256b94ab051046884114e0daded6eafdaaf
Jack He <heyingjie1993@outlook.com>
2016-09-29 16:24:02 -0700

    Leetcode

--- 66-Plus-One/solution.java
+++ 66-Plus-One/solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
         if(digits == null || digits.length == 0) return digits;
-        for(int i = digits.length - 1; i >= 0; i--){
-            if(digits[i] < 9){
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
