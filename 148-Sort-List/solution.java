05d33ec3d596e40fbb96b38695c534594a291f49
Jack He <heyingjie1993@outlook.com>
2016-09-29 12:14:06 -0700

    Leetcode

---
+++ 148-Sort-List/solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode pre = null, slow = head, fast = head;
+        while(fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        ListNode temp1 = sortList(head);
+        ListNode temp2 = sortList(slow);
+        return merge(temp1, temp2);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                head.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        if(l1 != null) head.next = l1;
+        if(l2 != null) head.next = l2;
+        return dummy.next;
+    }
+}
