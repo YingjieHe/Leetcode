db1a93cca0f6acd9c71c85351740da0b65ebecb1
Jack He <heyingjie1993@outlook.com>
2016-09-30 22:42:22 -0700

    Leetcode

--- 260-Single-Number-III/solution.java
+++ 260-Single-Number-III/solution.java
@@ -1,12 +1,11 @@
 public class Solution {
     public int[] singleNumber(int[] nums) {
         int[] res = new int[2];
-        if(nums == null || nums.length <= 1) return null;
         int xor = 0;
         for(int i : nums) xor ^= i;
         xor &= -xor;
-        for(int i : nums){
-            if((i & xor) == 0) res[0] ^= i;
+        for(int i : nums) {
+            if((xor & i) == 0) res[0] ^= i;
             else res[1] ^= i;
         }
         return res;
