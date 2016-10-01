7ad69919688fba624617ae2cb7ccd6a7391ad747
Jack He <heyingjie1993@outlook.com>
2016-10-01 10:48:38 -0700

    Leetcode

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -3,8 +3,8 @@ public class Solution {
         int[] res = new int[n + 1];
         res[0] = 1;
         res[1] = 1;
-        for(int i = 2; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
                 res[i] += res[j - 1] * res[i - j];
             }
         }
