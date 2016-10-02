48909c061bd8855cb5b40ad9488771ad1397653b
Jack He <heyingjie1993@outlook.com>
2016-10-01 20:33:58 -0700

    Leetcode

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,8 +1,9 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
         int max = nums[0];
         int sum = nums[0];
-        for(int i = 1; i < nums.length; i++){
+        for(int i = 1; i < nums.length; i++) {
             sum = Math.max(sum + nums[i], nums[i]);
             max = Math.max(sum, max);
         }
