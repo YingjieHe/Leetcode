fcb7b26edb4397c83154899264fc1b11be378adf
Jack He <heyingjie1993@outlook.com>
2016-10-01 22:09:25 -0700

    Leetcode

--- 34-Search-for-a-Range/solution.java
+++ 34-Search-for-a-Range/solution.java
@@ -1,24 +1,20 @@
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = {-1, -1};
-        if(nums == null || nums.length == 0) return res;
+        int[] res = new int[2];
+        res[0] = binarySearch(nums, target);
+        if(res[0] == nums.length || nums[res[0]] != target) return new int[]{-1, -1};
+        res[1] = binarySearch(nums, target + 1) - 1;
+        return res;
+    }
+    
+    private int binarySearch(int[] nums, int target) {
         int left = 0;
-        int right = nums.length - 1;
-        while(left < right){
+        int right = nums.length;
+        while(left < right) {
             int mid = left + (right - left) / 2;
             if(nums[mid] < target) left = mid + 1;
             else right = mid;
         }
-        if(nums[left] != target) return res;
-        res[0] = left;
-        left = 0;
-        right = nums.length - 1;
-        while(left < right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] < target + 1) left = mid + 1;
-            else right = mid;
-        }
-        res[1] = nums[left] == target? left: left - 1;
-        return res;
+        return left;
     }
 }
