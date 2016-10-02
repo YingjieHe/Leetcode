1a78aff1937ebf31bd30ec9c35575de46c63a793
Jack He <heyingjie1993@outlook.com>
2016-10-02 15:46:53 -0700

    Leetcode

---
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        if(tokens == null || tokens.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        for(String s : tokens) {
+            if(s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            }
+            else if(s.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b - a);
+            }
+            else if(s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            }
+            else if(s.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b / a);
+            }
+            else stack.push(Integer.parseInt(s));
+        }
+        return stack.pop();
+    }
+}
