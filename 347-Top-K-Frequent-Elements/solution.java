fd10882ddeb918629b64d90098e88e15e71bbe7d
Jack He <heyingjie1993@outlook.com>
2016-10-03 11:03:08 -0700

    Leetcode

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -1,20 +1,18 @@
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        List<Integer> res = new ArrayList<Integer>();
-        if(nums == null || nums.length == 0 || k <= 0) return res;
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i : nums){
-            if(map.containsKey(i)) map.put(i, map.get(i) + 1);
-            else map.put(i, 1);
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> p = new PriorityQueue<Map.Entry<Integer, Integer>>(new Comparator<Map.Entry<Integer, Integer>>(){
-            public int compare(Map.Entry<Integer, Integer> m1, Map.Entry<Integer, Integer> m2){
-                return m2.getValue() - m1.getValue();
-            }
-        });
-        p.addAll(map.entrySet());
-        for(int i = 0; i < k; i++){
-            res.add(p.poll().getKey());
+        for(int key : map.keySet()) {
+            int freq = map.get(key);
+            if(bucket[freq] == null) bucket[freq] = new ArrayList<Integer>();
+            bucket[freq].add(key);
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int i = bucket.length - 1; i >= 0 && res.size() < k; i--) {
+            if(bucket[i] != null) res.addAll(bucket[i]);
         }
         return res;
     }
