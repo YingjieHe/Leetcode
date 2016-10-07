03e27005514e92884c46e4b24832bba14514cb34
Jack He <heyingjie1993@outlook.com>
2016-10-07 13:26:14 -0700

    Leetcode

---
+++ 57-Insert-Interval/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList<>();
+        if(intervals == null || intervals.size() == 0) {
+            res.add(newInterval);
+            return res;
+        }
+        int i = 0;
+        while(i < intervals.size() && intervals.get(i).end < newInterval.start) {
+            res.add(intervals.get(i++));
+        }
+        while(i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval = new Interval(Math.min(newInterval.start, intervals.get(i).start),
+            Math.max(newInterval.end, intervals.get(i).end));
+            i++;
+        }
+        res.add(newInterval);
+        while(i < intervals.size()) {
+            res.add(intervals.get(i++));
+        }
+        return res;
+    }
+}
