4221dcdb08076076092d8ab339ae5a6799678c1d
Jack He <heyingjie1993@outlook.com>
2016-10-12 10:43:41 -0700

    Leetcode

--- 239-Sliding-Window-Maximum/solution.java
+++ 239-Sliding-Window-Maximum/solution.java
@@ -1,14 +1,16 @@
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if(nums == null || nums.length == 0) return new int[0];
-        Deque<Integer> q = new LinkedList<>();
         int[] res = new int[nums.length - k + 1];
+        Deque<Integer> q = new LinkedList<>();
         int index = 0;
         for(int i = 0; i < nums.length; i++) {
             while(!q.isEmpty() && q.peek() < i - k + 1) q.poll();
             while(!q.isEmpty() && nums[q.peekLast()] < nums[i]) q.pollLast();
             q.offer(i);
-            if(i >= k - 1) res[index++] = nums[q.peek()];
+            if(i >= k - 1) {
+                res[index++] = nums[q.peek()];
+            }
         }
         return res;
     }
